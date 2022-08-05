3cf0b28c0d0be8f7244e3476c22cee99d7e41d75
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-08-05 18:53:32 +0100

    QA(Tests): Add basic tests for all routes

--- routes/users/tests/users.test.js
+++ routes/users/tests/users.test.js
@@ -1,5 +1,6 @@
 const request = require('supertest');
 const server = require('../../../app');
+const Users = require('../models/Users');
 
 describe('Users Tests', () => {
     let api;
@@ -14,16 +15,27 @@ describe('Users Tests', () => {
     });
 
     test("successful requests to POST /users/register returns status '201 created'", done => {
+        Users.emailTaken = jest.fn();
+        Users.usernameTaken = jest.fn();
+        Users.createUser = jest.fn();
+
+        Users.emailTaken.mockReturnValue(false);
+        Users.usernameTaken.mockReturnValue(false);
+        Users.createUser.mockReturnValue('User successfully added.');
+
         const testUser = {
-            username: '123',
-            email: '123@example.com',
+            username: 'im new',
+            email: 'andme@example.com',
             password: '12345',
             isAdmin: true,
         };
         request(api).post('/users/register').send(testUser).expect(201, done);
     });
 
-    test("a request POST /users/register with existing username returns status '400 Bad Request'", done => {
+    test("request to POST /users/register with existing username returns status '400 Bad Request'", done => {
+        Users.usernameTaken = jest.fn();
+        Users.usernameTaken.mockReturnValue(true);
+
         const testUser = {
             username: '123',
             email: '123@example.com',
@@ -31,9 +43,39 @@ describe('Users Tests', () => {
             isAdmin: true,
         };
 
-        // Add user.
-        // Try to add user with the same username
+        request(api).post('/users/register').send(testUser).expect(400, done);
+    });
+
+    test("request to POST /users/register with existing email returns status '400 Bad Request'", done => {
+        Users.emailTaken = jest.fn();
+        Users.emailTaken.mockReturnValue(true);
+
+        const testUser = {
+            username: '123',
+            email: '123@example.com',
+            password: '12345',
+            isAdmin: true,
+        };
 
         request(api).post('/users/register').send(testUser).expect(400, done);
+    });
+
+    test("if createUser returns an error the client receives '503 status code'", done => {
+        Users.emailTaken = jest.fn();
+        Users.usernameTaken = jest.fn();
+        Users.createUser = jest.fn();
+
+        Users.emailTaken.mockReturnValue(false);
+        Users.usernameTaken.mockReturnValue(false);
+        Users.createUser.mockRejectedValue(new Error('Async error message'));
+
+        const testUser = {
+            username: '123',
+            email: '123@example.com',
+            password: '12345',
+            isAdmin: true,
+        };
+
+        request(api).post('/users/register').send(testUser).expect(503, done);
     });
 });
