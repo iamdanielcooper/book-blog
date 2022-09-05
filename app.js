595ee9a2aed5d3a2c866e2f3e55d3a3fa8459dda
Dan Cooper <iamdanielcooper@users.noreply.github.com>
2022-09-05 19:09:33 +0100

    BE(Database): update database routes to dev only

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Database Controls
-const databaseRoutes = require('./routes/database/databaseRoutes');
-app.use('/database', databaseRoutes);
+if (process.env.ENV === 'DEV') {
+    const databaseRoutes = require('./routes/database/databaseRoutes');
+    app.use('/database', databaseRoutes);
+}
 
 // Users
 const usersRoutes = require('./routes/users/usersRoutes');
